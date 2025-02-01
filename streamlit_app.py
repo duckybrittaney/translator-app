fa8054e0002f793ac70c6d56c536c90f043baa05
duckybrittaney <brittaneyjin@gmail.com>
2025-02-01 23:34:14 +0000

    completed assignment

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,45 @@
 import streamlit as st
+import json
+from openai import OpenAI
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+api_key = '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'
+client = OpenAI(api_key=api_key)
+system_prompt = '''
+You are a language translator. A user will give you an input language, an output language, and the text they want translated. Output ONLY their translated message.
+'''
+
+def chat(inp,outp,txt):
+    user_prompt = f'{inp} -> {outp}, my message is: {txt}'
+    response = client.chat.completions.create(model = 'gpt-3.5-turbo-0125',
+    messages = [
+        {'role': 'system', 'content': system_prompt},
+        {'role': 'user', 'content': user_prompt},
+    ])
+    if submit:
+        st.write('Translated text:')
+        st.write(response.choices[0].message.content)
+
+st.title("Translator")
+
+with st.form('my_form'):
+    input_lan = st.selectbox(
+        "Select an input language",
+        [
+            "Spanish",
+            "English",
+            "Italian",
+        ]
+    )
+    output_lan = st.selectbox(
+        "Select an output language",
+        [
+            "Mandarin",
+            "French",
+            "German",
+        ]
+    )
+    text = st.text_input("Type in the text to translate")
+
+
+    submit = st.form_submit_button('Submit')
+    chat(input_lan, output_lan, text)
