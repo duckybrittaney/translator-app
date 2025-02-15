665ed558f2ab4d13130172617486e46f7d30c944
duckybrittaney <brittaneyjin@gmail.com>
2025-02-15 22:37:02 +0000

    api key

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import json
 from openai import OpenAI
 
-api_key = st.secrets["api"]["key"]
+api_key = st.secrets["openai"]["api_key"]
 client = OpenAI(api_key=api_key)
 system_prompt = '''
 You are a language translator. A user will give you an input language, an output language, and the text they want translated. Output ONLY their translated message.
